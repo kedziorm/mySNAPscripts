a4736b6d1f5731f80fd2528dfdceda995f686cfa
Mateusz Kędzior <Mateusz.Kedzior@is.pw.edu.pl>
2016-10-22 16:15:21 +0200

    Get file size

--- myScripts.py
+++ myScripts.py
@@ -177,6 +177,25 @@ def writeToLog(message, messageType='ERROR'):
 	logger.removeHandler(hdlr)
 
 
+def convert_bytes(num):
+	"""
+	this function will convert bytes to MB.... GB... etc
+	"""
+	for x in ['bytes', 'KB', 'MB', 'GB', 'TB']:
+		if num < 1024.0:
+			return "%3.1f %s" % (num, x)
+		num /= 1024.0
+
+
+def file_size(file_path):
+	"""
+	this function will return the file size
+	"""
+	if os.path.isfile(file_path):
+		file_info = os.stat(file_path)
+		return convert_bytes(file_info.st_size)
+
+
 def getSigma(SentinelFile):
 	# calculate sigma (radar backscatter)
 	# in ESA SNAP desktop: Radar --> Radiometric --> Calibrate
